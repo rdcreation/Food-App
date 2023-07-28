78ca33913f1a585c575b77cbb8a4e4fe68ac4fd7
Rajesh Devaraj <rajesh.devaraj@eleviant.com>
2023-07-28 16:16:18 +0530

    meals added

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,25 +5,25 @@ import classes from './AvailableMeals.module.css';
 const DUMMY_MEALS = [
   {
     id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
+    name: 'Burger',
+    description: 'veggies burger',
     price: 22.99,
   },
   {
     id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
+    name: 'Briyani',
+    description: 'Hyderbad Briyani!',
     price: 16.5,
   },
   {
     id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
+    name: 'Fired Rice',
+    description: 'Egg Fried rice with spicy',
     price: 12.99,
   },
   {
     id: 'm4',
-    name: 'Green Bowl',
+    name: 'Pizza',
     description: 'Healthy...and green...',
     price: 18.99,
   },
